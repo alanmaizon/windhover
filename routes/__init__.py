06346b15944cf079c7bb9beaeb7068aca07d4f13
Alan Maizon <alanmaizon@hotmail.com>
2024-10-23 23:16:06 +0100

    Key Changes of SQLAlchemy

--- routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from routes.books import books_bp
-from routes.members import members_bp
-from routes.borrowing import borrowing_bp
-
-def init_routes(app):
-    app.register_blueprint(books_bp)
-    app.register_blueprint(members_bp)
-    app.register_blueprint(borrowing_bp)
